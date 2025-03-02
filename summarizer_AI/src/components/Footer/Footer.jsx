a553a44fee69a02b599487735a06b2f33ab29008
Yasara <madanayasara@gmail.com>
2025-03-02 18:17:00 +0530

    UI Fixing
    suitable UI

--- summarizer_AI/src/components/Footer/Footer.jsx
+++ summarizer_AI/src/components/Footer/Footer.jsx
@@ -4,27 +4,30 @@ import logo from '../../assests/images/logo.png';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-left">
-        <img src={logo} alt="StudyHive" className="footer-logo" />
-        <h2 className="footer-brand">StudyHive</h2>
+    <footer className="las_footer">
+      <div className="las_footer-left">
+        <img src={logo} alt="StudyHive" className="las_footer-logo" />
+        <h2 className="las_footer-brand">StudyHive</h2>
       </div>
-      <div className="footer-center">
+      <div className="las_footer-center">
         <a href="/about">About</a>
         <a href="/features">Features</a>
         <a href="/feedback">Feedback</a>
         <a href="/donate">Donate</a>
         <a href="/team">Team</a>
       </div>
-      <div className="footer-right">
-        <a href="https://www.facebook.com/profile.php?id=61570160061839" target="_blank" rel="noopener noreferrer" className="social-link">
-          <i className="fa-brands fa-facebook-f"></i>
+      <div className="las_footer-right">
+        <a href="https://www.facebook.com/profile.php?id=61570160061839" className="social-link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook"></i>
         </a>
-        <a href="https://www.instagram.com/studyhive_edu/profilecard/" target="_blank" rel="noopener noreferrer" className="social-link">
-          <i className="fa-brands fa-instagram"></i>
+        <a href="https://www.instagram.com/studyhive_edu/profilecard/?igsh=Zmo1cHlrc3E5dGht" className="social-link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
         </a>
-        <a href="https://www.linkedin.com/company/studyhive/" target="_blank" rel="noopener noreferrer" className="social-link">
-          <i className="fa-brands fa-linkedin-in"></i>
+        <a href="https://www.linkedin.com/company/studyhive/" className="social-link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://github.com/StudyHive-CS47" className="social-link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
         </a>
       </div>
     </footer>
