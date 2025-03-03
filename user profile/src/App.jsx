291bb632144e3ed54c2ffbcf57a82af450d297bd
Rashmina Fernando <fdo.rashmina@gmail.com>
2025-03-03 10:06:12 +0530

    uploading the initial user profile files

---
+++ user profile/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LoginPage from './pages/Login/LoginPage';
+import SignupPage from './pages/Signup/SignupPage';
+import SuccessPage from './pages/Success/SuccessPage';
+import ConnectionStatus from './components/ConnectionStatus/ConnectionStatus';
+import ErrorBoundary from './components/ErrorBoundary';
+import ProfilePage from './pages/Profile/ProfilePage';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import './App.css';
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <Router>
+        <AuthProvider>
+          <div className="app">
+            <ConnectionStatus />
+            <Header />
+            <main className="main-content">
+              <React.Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/signup" element={<SignupPage />} />
+                  <Route 
+                    path="/profile" 
+                    element={
+                      <PrivateRoute>
+                        <ProfilePage />
+                      </PrivateRoute>
+                    } 
+                  />
+                  <Route path="/" element={<LoginPage />} />
+                </Routes>
+              </React.Suspense>
+            </main>
+            <Footer />
+          </div>
+        </AuthProvider>
+      </Router>
+    </ErrorBoundary>
+  );
+}
+
+export default App; 
---
+++ user profile/src/data/universities.js
@@ -0,0 +1,17 @@
+export const universities = [
+  "University of Colombo",
+  "University of Peradeniya",
+  "University of Sri Jayewardenepura",
+  "University of Kelaniya",
+  "University of Moratuwa",
+  "University of Jaffna",
+  "University of Ruhuna",
+  "Open University of Sri Lanka",
+  "Eastern University of Sri Lanka",
+  "South Eastern University of Sri Lanka",
+  "Rajarata University of Sri Lanka",
+  "Sabaragamuwa University of Sri Lanka",
+  "Wayamba University of Sri Lanka",
+  "Uva Wellassa University",
+  "University of the Visual & Performing Arts"
+]; 
