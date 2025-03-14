9f81cfc8aacdea2058cc2633f7dcc8d9a2189c45
Yasara <madanayasara@gmail.com>
2025-03-14 13:15:05 +0530

    chat remove
    remove

--- GroupChatImplement - Copy/backend/src/main/java/com/groupchat/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.groupchat.backend.repository;
-
-import com.groupchat.backend.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-}
