3cb609e7f019234d74b57b90cf58f6ae69e59b67
Yasara <madanayasara@gmail.com>
2025-03-18 20:56:51 +0530

    paths
    for now

---
+++ studyhive-monorepo/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./packages/**/*.{js,jsx,ts,tsx}",
+    "./packages/features/**/*.{js,jsx,ts,tsx}",
+    "./packages/shared/**/*.{js,jsx,ts,tsx}",
+    "./packages/auth/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
