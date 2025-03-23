e64aee16ad3c1cb3b650c1e90e4f35d999109074
Thevindu Jayakody <jayakodythevindu@gmail.com>
2025-03-24 01:06:38 +0530

    Update ChatBot.jsx

--- chat_bot/src/components/ChatBot.jsx
+++ chat_bot/src/components/ChatBot.jsx
@@ -11,7 +11,7 @@ const ChatBot = () => {
   const [messages, setMessages] = useState([
     {
       role: 'assistant',
-      content: "**WELCOME! TO BUZZBUDDY**",
+      content: "**WELCOME TO BUZZBUDDY**",
       timestamp: new Date().toLocaleString('en-US', {
         hour: '2-digit',
         minute: '2-digit',
